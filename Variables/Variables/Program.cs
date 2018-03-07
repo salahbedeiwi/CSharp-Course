53195bc77b25e57dc6f334a321b47e3680911a44
Salah Bedeiwi <12550747+salahbedeiwi@users.noreply.github.com>
2018-03-06 23:14:24 -0600

    add more

---
+++ Variables/Variables/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    class Program
+    {
+        /// <summary>
+        /// Block of code goes here
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            //Define Variables
+            byte num1, num2, num3;
+            string fName, lName, City;
+            char c,d,e;
+            bool overAge, underAge;
+            double cost;
+            const int totalMonth = 12; // Can't override this constant
+            const string developer = "Salah Bedeiwi";
+            const int weeks = 52;
+            const double pi = 3.14;
+            const int speedOfLight = 300000; //300km/s
+            const int minAgeToApply = 18;
+
+             
+            //Assign Variable
+            num1 = 1; //only one positive number
+            c = 'C';
+            overAge = true;
+            underAge = false;
+            cost = 11.5;
+
+            //Data Type Ranges:
+
+
+            Console.WriteLine("Welcome!");
+            Console.WriteLine(weeks);
+
+        }
+    }
+}
