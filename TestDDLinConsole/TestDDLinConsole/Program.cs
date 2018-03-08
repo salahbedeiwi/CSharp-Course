032567dc627271a2ef3a2c5757da6442df1e4da6
Salah Bedeiwi <12550747+salahbedeiwi@users.noreply.github.com>
2018-03-08 17:45:52 -0600

    How to add a reference or dll file to your project
    How to add a reference to your file, make your project light.
    
    if you have a file with a lot of classes that could be helpful for your project, simple add it as a reference
    To do so, 
    
    take the dll file from debug folder inside your main project after your build it.
    now take that file to the new project and add a reference for it, then add the name space inside your new project.
    therefore you will be able to access all classes, methods, .... but must be public so you can use it.

---
+++ TestDDLinConsole/TestDDLinConsole/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using CreateClassDDLinClassLibrary;
+//call the ddl using CreateClassDDLinClassLibrary; //call the ddl Create......dll 
+//- will access all public classes and anything thre
+//all you need to do, build you app, then get the .dll file and import it here by
+//using the namespace of that dll file
+//go to reference inside this project under the properties.
+//you will see the CreateClassDDLinClassLibrary there with all classes inside
+namespace TestDDLinConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //i can access anything inside that file --- wooooooow
+            //this is exactly how c# work - saves everything on dll files
+            //give it namesspaces, like using system, .... etc
+            //you will have an access to everything. light files, ...
+
+            Employee e = new Employee();
+            e.address = "361";
+            e.phone = "6126441634";
+            e.printEmployeeData();
+            Console.ReadLine();
+        }
+    }
+}
