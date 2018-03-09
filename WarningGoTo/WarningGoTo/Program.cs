704207902c9eb922c07af0b97630abd086f4e783
Salah Bedeiwi <12550747+salahbedeiwi@users.noreply.github.com>
2018-03-09 13:59:44 -0600

    How to use Spaghiti programming using goto

---
+++ WarningGoTo/WarningGoTo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarningGoTo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            start:; //i customed something that i can go back at any tim
+            Console.WriteLine( "Enter your name => ");
+            string name = Console.ReadLine();
+            Console.WriteLine( "Your name is {0}", name );
+            //ask him if we need to enter your name again?
+            Console.WriteLine("Enter your name again => (y/n)");
+            char c = Console.ReadLine()[0]; //get first 1 char only
+            if (c == 'y' || c == 'Y') goto start;
+            Console.ReadKey();
+
+        }
+    }
+}
