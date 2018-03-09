9e7000e61347ec630ce81c1b6970257f4090935c
Salah Bedeiwi <12550747+salahbedeiwi@users.noreply.github.com>
2018-03-08 22:50:07 -0600

    How to be directed to a website by class

---
+++ ClassToGoToWebsite/ClassToGoToWebsite/GoToWebsite.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassToGoToWebsite
+{
+    class GoToWebsite
+    {
+        private delegate void _ToWeb(string url);
+        private static void Run(string url)
+        {
+            //i can also validate the url to make sure it is url
+            System.Diagnostics.Process.Start(url);
+        }
+        public static void ToWeb(string url)
+        {
+            //#1: way to do it: open url, filename, ...
+            //System.Diagnostics.Process.Start(url);
+
+            //#2: second way:
+            _ToWeb toweb = Run; //assign the delegate to Run method
+            toweb(url);
+        }
+    }
+}
---
+++ ClassToGoToWebsite/ClassToGoToWebsite/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassToGoToWebsite
+{
+    //create class to go to new website
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GoToWebsite.ToWeb("www.google.com");
+            GoToWebsite.ToWeb("../");
+            Console.ReadKey();
+        }
+    }
+}
