cfc57097c3f6094f2ce6025283831bce75273d67
Salah Bedeiwi <12550747+salahbedeiwi@users.noreply.github.com>
2018-03-09 00:00:45 -0600

    Pass inifinite params using param keyword

---
+++ InfiniteParam/InfiniteParam/MyMath.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfiniteParam
+{
+    class MyMath
+    {
+        public static int SumParam(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        public static int SumParam(int num1, int num2, int num3)
+        {
+            return num1 + num2 + num3;
+        }
+        public static int SumParam(int num1, int num2, int num3, int num4)
+        {
+            return num1 + num2 + num3 + num4;
+        }
+        //imagine i have 100's of parameters, i can't write every single param
+        public static int SumAnyParam(int[] sum)
+        {
+            int total = 0;
+            foreach(int s in sum)
+            { //if numbers and int type
+                if(s.GetType().Name == "Int32")
+                {
+                        total += s;
+                }else
+                {
+                    return total = 0;
+                }
+            }
+            return total;
+
+        }
+        //best way: instead of passing an array, you can directly pass param as variables
+        public static int SumOfAll(params int[] numbers)
+        {
+            int sum = 0;
+            foreach (int s in numbers)
+            {
+                if(s.GetType().Name == "Int32")
+                {
+                    sum += s;
+                }
+                else
+                {
+                    sum = 0;
+                }
+            }
+            return sum;
+        }
+        public static string ConcatinateString(params string[] x)
+        {
+            string y = "";
+            foreach(string s in x)
+            {
+                if (s.GetType().Name == "String")
+                    y += s+" ";
+                else
+                    y = "";
+            }
+            return y;
+        }
+    }
+    
+}
---
+++ InfiniteParam/InfiniteParam/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfiniteParam
+{
+    //class accepts many parameters
+    //create a method that takes as many as numbers and add them
+
+    class Program
+    {
+        static void PL(int n) {
+            string x = "";
+            for(int f = 0; f < n; f++)
+            {
+                x += "=";
+            }
+            Console.WriteLine(x);
+        }
+        static void Main(string[] args)
+        {
+            int[] x = new int[] { 12, 12, 12, 12, 12, 12 };
+            int z = MyMath.SumAnyParam(x);
+            Console.WriteLine("Total is {0}", z);
+            PL(80);
+
+            int[] x32 = new int[] { 2, 1, 3, 4, 5, 6 };
+            int z32 = MyMath.SumAnyParam(x32);
+            Console.WriteLine("Total is {0}", z32);
+            PL(80);
+
+            Console.WriteLine("Sum is {0}", MyMath.SumParam(15, 16));
+            Console.WriteLine("Sum is {0}", MyMath.SumParam(15, 16, 152));
+            PL(80);
+
+            //use params keyword, easiest
+            Console.WriteLine("Sum is => " + 
+                                    MyMath.SumOfAll(15, 32, 32, 32, 120, 326, 32));
+            PL(80);
+            Console.WriteLine("Sum is => " +
+                                    MyMath.SumOfAll(15));
+            Console.WriteLine("===================================");
+            Console.WriteLine("Sum is => " +
+                                    MyMath.SumOfAll(15, -32, +32));
+            Console.WriteLine("===================================");
+            //i can also pass an array
+            Console.WriteLine("Sum is => " +
+                                    MyMath.SumOfAll(new int[] { 2, 1, 3, 4, 5, 6 }));
+            Console.WriteLine("===================================");
+            Console.WriteLine("Sum is => " +
+                                    MyMath.SumOfAll(z32)); //pass array
+            PL(80);
+            //note, i can pass any variables as string as well
+            Console.WriteLine("String of full name: {0}"
+                                , MyMath.ConcatinateString("Salah", "Imad", "Bedeiwi"));
+            PL(40+40);
+            Console.ReadKey();
+        }
+    }
+}
+/* Answers:
+ Total is 72
+================================================================================
+Total is 21
+================================================================================
+Sum is 31
+Sum is 183
+================================================================================
+Sum is => 589
+================================================================================
+Sum is => 15
+===================================
+Sum is => 15
+===================================
+Sum is => 21
+===================================
+Sum is => 21
+================================================================================
+String of full name: Salah Imad Bedeiwi
+================================================================================
+
+
+*/
