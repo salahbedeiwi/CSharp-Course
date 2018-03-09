2791c990ce9dd17658debb9cd7622f486f3efabc
Salah Bedeiwi <12550747+salahbedeiwi@users.noreply.github.com>
2018-03-08 21:26:23 -0600

    Access Modifiers

---
+++ AccessModifier/AccessModifier/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessModifier
+{
+    class Program
+    {
+        public int x;
+        private int x2;
+        protected internal int x3;
+        protected int x4;
+        internal int x5;
+        int x6;
+        static void Main(string[] args)
+        {
+        }
+    }
+}
