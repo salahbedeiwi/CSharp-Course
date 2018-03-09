5b40b43e28a0046ea97e7ca7cd5afa45fcf2b0b3
Salah Bedeiwi <12550747+salahbedeiwi@users.noreply.github.com>
2018-03-09 15:13:50 -0600

    How to use recursive function
    call a function within the same function

---
+++ RecursiveFunction/RecursiveFunction/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecursiveFunction
+{
+    //class that have a recursive function:
+    //factorical: n! => 3! => 3*2*1 is 6
+    class MyMath
+    {
+        public static int Factorial(int num)
+        {
+            if (num == 0) return 1; //0! = 1
+            else return num * Factorial(num - 1);
+            //note i can call same method inside
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = MyMath.Factorial(3);
+            Console.WriteLine(i);
+            Console.ReadKey();
+        }
+    }
+}
