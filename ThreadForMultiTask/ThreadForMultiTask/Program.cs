938745a8f1c4208c6746ed6d3edf5c4a9c811670
Salah Bedeiwi <12550747+salahbedeiwi@users.noreply.github.com>
2018-03-10 01:13:36 -0600

    lock, Thread, Sleep, Wait

--- ThreadForMultiTask/ThreadForMultiTask/Program.cs
+++ ThreadForMultiTask/ThreadForMultiTask/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-
+using static ThreadForMultiTask.WaitForSeconds; //use this for calling the method right away.
 namespace ThreadForMultiTask
 {
     //imagine you have a departement that has 20 employees
@@ -24,10 +24,36 @@ namespace ThreadForMultiTask
             for (int x = 0; x < 10; x++) Console.WriteLine("Run Two"); //print 10 times
         }
     }
+    class WaitForSeconds
+    {
+        public static void Wait(int seconds)
+        {
+            Thread.Sleep(seconds * 1000);
+        }
+        public static void WaitInMs(int seconds)
+        {
+            Thread.Sleep(seconds);
+        }
+    }
     class Program
     {
+        //use lock Threading
+        static object obj = new object();
+        static void PrintNumbers()
+        {
+            //note, use lock to make sure the Thread t = new Thread(method), t.Start(); work fine
+            //must use lock
+            //lock very much, it looks for any Thread out there, and check if any Thread have
+            // a conflict with it, so it is safer
+            lock (obj) //lock (""), pass anything
+            {
+                WaitInMs(100);//wait for .1 second
+                Console.WriteLine("0123456789");
+            }
+        }
         static void Main(string[] args)
         {
+            
             HelloOne h1 = new HelloOne();
             HelloTwo h2 = new HelloTwo();
             h1.Run();
@@ -45,14 +71,43 @@ namespace ThreadForMultiTask
             t1.Start();
             t2.Start();
             //look at results.
+            //idea: to create a multi task to be excuted at the same time.
+            Console.WriteLine("===================================================");
 
-            //idea: create a multi task to be excuted at the same time.
+            //can i make my thread(task) sleep or wait???
+            //whenever you call something, let it wait for a while # of seconds
+            Thread.Sleep(4000); //wait for 4 secs
+            Console.WriteLine("You have waited for 4 seconds");
+            WaitForSeconds.Wait(5);
+            Console.WriteLine("You have waited for 5 seconds");
+            //note i will use => using static ThreadForMultiTask.WaitForSeconds
+            Wait(2);//wait for 2 seconds
+            Console.WriteLine("You have waited for 2 seconds");
+            Wait(3);
+            Console.WriteLine("You have waited for 3 seconds");
+            Wait(2);
+            Console.WriteLine("===================================================");
+            Console.WriteLine("You have waited for 2 seconds");
+            Wait(1);
+            Console.WriteLine("You have waited for 1 seconds");
+            Wait(0);
+            Console.WriteLine("You have waited for 0 seconds");
             
+            //lock in Threading: means print it in time manner
+            for (int i = 0; i < 10; i++)
+            {
+                Thread t = new Thread(PrintNumbers);
+                t.Start();
+                //or
+                //PrintNumbers();
+
+            }
             Console.ReadKey();
         }
     }
 }
-/*Run One
+/*
+Run One
 Run One
 Run One
 Run One
@@ -73,9 +128,8 @@ Run Two
 Run Two
 Run Two
 ===================================================
-Run Two
-Run Two
-Run Two
+Run One
+===================================================
 Run One
 Run One
 Run One
@@ -92,5 +146,26 @@ Run Two
 Run Two
 Run Two
 Run Two
-Run One
+Run Two
+Run Two
+Run Two
+You have waited for 4 seconds
+You have waited for 5 seconds
+You have waited for 2 seconds
+You have waited for 3 seconds
+===================================================
+You have waited for 2 seconds
+You have waited for 1 seconds
+You have waited for 0 seconds
+0123456789
+0123456789
+0123456789
+0123456789
+0123456789
+0123456789
+0123456789
+0123456789
+0123456789
+0123456789
+
 */
