1c4e08f161eb7234b56612457701b18e1b115915
Salah Bedeiwi <12550747+salahbedeiwi@users.noreply.github.com>
2018-03-09 21:17:45 -0600

    Check for max int value

---
+++ MethodCheckIntNum/MethodCheckIntNum/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodCheckIntNum
+{
+    class Program
+    {
+        static void CheckMaxInt()
+        {
+            try
+            {
+                checked
+                {
+                    int i = 1;
+                    int i2 = checked(2147483647 + i);
+                    Console.WriteLine(i2);
+                }
+            }catch(Exception ex)
+            {
+                Console.WriteLine("Something went wrong with the max size of check! "
+                                + ex.Message);
+            }
+        }
+        static void Main(string[] args)
+        {
+
+            //usually if you suspect any code, use checked
+            //checked: shows error if something shows up
+            //unchecked: hide the error on run time
+            CheckMaxInt();
+            Console.ReadKey();
+        }
+    }
+}
