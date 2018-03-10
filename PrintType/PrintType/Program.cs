4d7c031d1eedcfe386e49b6a634b5b50ea57e2ca
Salah Bedeiwi <12550747+salahbedeiwi@users.noreply.github.com>
2018-03-09 22:09:09 -0600

    Print any type in a method
    print any type in a method using 
    is keyword, typeof, varName.GetType().Name

---
+++ PrintType/PrintType/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintType
+{
+    class Program
+    {
+        //create a method to get the type of anything
+        static void PrintType(object obj)
+        {
+            if (obj is int) Console.WriteLine("Integer");
+            else if (obj is decimal) Console.WriteLine("Decimal");
+            else if (obj is string) Console.WriteLine("String/Text");
+            else if (obj is char) Console.WriteLine("Char/Text");
+            else if (obj is bool) Console.WriteLine("Boolean");
+            else if (obj is double || obj is float) Console.WriteLine("Float/Double");
+            else Console.WriteLine(obj.GetType().Name);
+        }
+        static void Main(string[] args)
+        {
+            int i = 77;
+            double d = 99.55;
+            string s = "hi";
+            char c = 'e';
+            bool f = true;
+            Console.WriteLine("==========================================");
+            PrintType(f);
+            PrintType(c);
+            PrintType(s);
+            PrintType(d);
+            PrintType(i);
+            Console.WriteLine("==========================================");
+            Console.WriteLine(d.GetType().Name);
+            Console.WriteLine(typeof(int));
+            Console.WriteLine("==========================================");
+            Console.ReadKey();
+        }
+    }
+}
