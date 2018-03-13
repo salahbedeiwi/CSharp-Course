0804ba616fae6ca0f2f4994dc3e326a78d3b715e
Salah Bedeiwi <12550747+salahbedeiwi@users.noreply.github.com>
2018-03-13 09:58:10 -0500

    Build Custom Data from class to another
    How to use other class data and assign it to your new class data?

---
+++ CustomDataInClass/CustomDataInClass/Person.cs
@@ -0,0 +1,56 @@
+﻿using System;
+namespace CustomDataInClass
+{
+    public struct PersonData
+    {
+        //can't have empty struct (parameterless)
+        private int _id;
+        public int ID { get { return _id; } set { _id = value; } }
+        private string _name;
+        public string NAME { get { return _name; } set { _name = value; } }
+        private string _address;
+        public string Address { get { return _address; } set { _address = value; } }
+        private Prizes _prizes;
+        public Prizes PRIZES { get { return _prizes; } set { _prizes = value; } }
+    }
+    public enum Prizes { Bronze_Medal, Gold_Medal, Silver_Medal, Brown_Medal }
+    class Person
+    {
+        //get info PersonData. Note no inheritance
+        //idea is to use the properties and values on Person Class from PersonData 
+        //to the Person class. Done by implicit operator. make two class types same
+        //pass parameter, means:
+        //get all data from that object and access it here.
+        public static implicit operator Person(PersonData data)
+        {
+            Person p = new Person();
+            p.ID = data.ID;
+            p.NAME = data.NAME;
+            p.Address = data.Address;
+            p.PRIZES = data.PRIZES;
+            return p; //must return
+        }
+        private int _id;
+
+        public int ID { get { return _id; } set { _id = value; } }
+        private string _name;
+
+        public string NAME { get { return _name; } set { _name = value; } }
+        private string _address;
+
+        public string Address { get { return _address; } set { _address = value; } }
+        private Prizes _prizes;
+        public Prizes PRIZES { get { return _prizes; } set { _prizes = value; } }
+
+        private string GetData()
+        {
+            this._id = ID;
+            this._name = NAME;
+            this._address = Address;
+            this._prizes = PRIZES;
+            return string.Format("ID: {0}\nName: {1}\nAddress: {2}\nPrizes: {3}\n"
+                ,  _id, _name, _address, _prizes);
+        }
+        public void PrintData() { Console.WriteLine(GetData()); }
+    }
+}
---
+++ CustomDataInClass/CustomDataInClass/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomDataInClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //custom your class data:
+            Console.WriteLine("================================================");
+            PersonData pd = new PersonData();
+            pd.NAME = "Salah Bedeiwi";
+            pd.Address = "Minnesota, USA";
+            pd.ID = 1;
+            pd.PRIZES = Prizes.Bronze_Medal;
+            Console.WriteLine("================================================");
+            PersonData pd_2 = new PersonData();
+            pd_2.NAME = "Ahmed E";
+            pd_2.Address = "Minnesota, USA";
+            pd_2.ID = 3;
+            pd_2.PRIZES = Prizes.Brown_Medal;
+            Console.WriteLine("================================================");
+            Person p = new Person();
+            p.NAME = pd.NAME;
+            p.Address = pd.Address;
+            p.ID = pd.ID;
+            p.PRIZES = pd.PRIZES;
+            p.PrintData();
+            Console.WriteLine("================================================");
+            //shortcut and most fun part:
+            p = pd; //means all data are the same - remember implicit class
+            p.PrintData();
+            Console.WriteLine("================================================");
+            Person p2 = new Person();
+            p2 = pd_2;
+            p2.PrintData();
+            Console.WriteLine("================================================");
+            //We know how to do the following. Now the question is how to
+            Person p0 = new Person();
+            p0.NAME = "Mohamed E";
+            p0.Address = "Minnesota, USA";
+            p0.ID = 7;
+            p0.PRIZES = Prizes.Gold_Medal;
+            p0.PrintData();
+            Console.WriteLine("================================================");
+
+            Console.ReadKey();
+        }
+    }
+}
